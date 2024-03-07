ad56078ebb5ca089b950523a2b8e5dc434597af9
JoseAngelgomezm <joseangelgmj@hotmail.com>
2024-03-07 17:45:09 +0100

    agrega cambios para ver si funciona jenkin

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,8 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
+        
+        Aqui hay un cambio que he realizado para ver si funciona el pipeline de jenkins
       </p>
     </>
   )
